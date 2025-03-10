10be43ebdc368615138e984b028fcfa8c4990bc1
Shalini Nayan <shalininayan@outlook.com>
2025-03-10 22:58:03 +0530

    Add and update the function for footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+    return (
+      <footer className="bg-gray-900 text-white p-4 text-center mt-10">
+        © {new Date().getFullYear()} My Blog | All Rights Reserved
+      </footer>
+    );
+  }
+  
